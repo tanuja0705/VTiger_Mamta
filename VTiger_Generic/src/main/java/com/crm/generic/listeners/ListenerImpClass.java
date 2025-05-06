080020f2dac351acbc9911ddf38dd573b2259121
Mamathargowda <mamathagowda176@gmail.com>
2025-05-06 11:00:26 +0530

    changed config data

--- VTiger_Generic/src/main/java/com/crm/generic/listeners/ListenerImpClass.java
+++ VTiger_Generic/src/main/java/com/crm/generic/listeners/ListenerImpClass.java
@@ -21,56 +21,56 @@ public class ListenerImpClass implements ITestListener, ISuiteListener {
 	public ExtentSparkReporter spark;
 	public ExtentReports report;
 	public ExtentTest test;
-	
+
 	public void onStart(ISuite suite) {
-	    System.out.println("Suite starts");
-	    String time = new Date().toString().replace(" ", "_").replace(":","_");
-	    spark = new ExtentSparkReporter("./AdvanceReport/report"+time+".html");
+		System.out.println("Suite starts");
+		String time = new Date().toString().replace(" ", "_").replace(":","_");
+		spark = new ExtentSparkReporter("./AdvanceReport/report"+time+".html");
 		spark.config().setDocumentTitle("Vtiger");
 		spark.config().setReportName("VtigerReport");
 		spark.config().setTheme(Theme.DARK);
-		
+
 		report = new ExtentReports();
 		report.attachReporter(spark);
 		report.setSystemInfo("browser", "chrome");
 		report.setSystemInfo("os", "windows-10");
 	}
-	
+
 	public void onFinish(ISuite suite) {
 		System.out.println("Suite ends");
 		report.flush();
 	}
-	
+
 	public void onTestStart(ITestResult result) {
 		System.out.println("Test starts");
 		test = report.createTest(result.getMethod().getMethodName());
 		ThreadLocalObject.setTest(test);
 		ThreadLocalObject.getTest().log(Status.INFO, result.getMethod().getMethodName()+"=====started======");
-		
+
 	}
-	
+
 	public void onTestSuccess(ITestResult result) {
 		System.out.println("Test Succeed");
 	}
-	
+
 	public void onTestFailure(ITestResult result) {
-	    String TestName = result.getMethod().getMethodName();
-	    TakesScreenshot tks = (TakesScreenshot) ThreadLocalObject.getDriver();
-	    String src = tks.getScreenshotAs(OutputType.BASE64);
-	    String time = new Date().toString().replace(" ", "_").replace(":","_");
-	    ThreadLocalObject.getTest().addScreenCaptureFromBase64String(src, TestName+"_"+time);
-	    ThreadLocalObject.getTest().log(Status.FAIL, result.getThrowable());
-	   
-	  }
-	
+		String TestName = result.getMethod().getMethodName();
+		TakesScreenshot tks = (TakesScreenshot) ThreadLocalObject.getDriver();
+		String src = tks.getScreenshotAs(OutputType.BASE64);
+		String time = new Date().toString().replace(" ", "_").replace(":","_");
+		ThreadLocalObject.getTest().addScreenCaptureFromBase64String(src, TestName+"_"+time);
+		ThreadLocalObject.getTest().log(Status.FAIL, result.getThrowable());
+
+	}
+
 	public void onTestSkipped(ITestResult result) {
 		System.out.println("Test Skipped");
 	}
-	
+
 	public void onStart(ITestContext context) {
 		System.out.println("Testtag starts");
 	}
-	
+
 	public void onFinish(ITestContext context) {
 		System.out.println("Testtag Ends");
 	}
